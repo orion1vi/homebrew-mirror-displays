414129a0c3d39b2f8daad9cea111f1d6a320fd58
Vilius <orion1vi@protonmail.com>
2020-01-11 21:29:13 +0200

    Modified formula

--- formula/mirror-displays.rb
+++ formula/mirror-displays.rb
@@ -5,11 +5,7 @@ class MirrorDisplays < Formula
   sha256 "3a44b1e65fdbcd15ba93ec1a1af97e205c8d274cb0272a40940add9f62853e2f"
   head "https://github.com/fcanas/mirror-displays.git"
 
-  depends_on :xcode => :build
-
   def install
-    # Set to build with SDK=macosx10.6, but it doesn't actually need 10.6
-    xcodebuild "SDKROOT=", "SYMROOT=build"
-    bin.install "build/Release/mirror"
+    bin.install "mirror"
   end
 end
